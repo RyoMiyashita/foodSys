d6e8df2181645c5fbe6facde1b466c8a6e2bc247
RyoMiyashita <ryomiyashita@RyoMiyashitanoMacBook-Air.local>
2018-01-26 23:12:37 +0900

    ログインできるようになった

---
+++ foods.rb
@@ -0,0 +1,181 @@
+require 'sinatra'
+require 'active_record'
+require 'digest/md5'
+require 'date'
+
+set :environment, :production
+set :sessions,
+  expire_after: 7200,
+  secret: 'gabagabasecretcodedesuw'
+
+
+ActiveRecord::Base.configurations = YAML.load_file('database.yml')
+ActiveRecord::Base.establish_connection :development
+
+class User < ActiveRecord::Base
+  has_many :foods
+end
+
+class Food < ActiveRecord::Base
+  belongs_to :user
+end
+
+get '/' do
+  redirect '/login'
+end
+
+get '/menu' do
+  if session[:login_flag] == true
+    @userName = session[:user_name]
+    erb :menu
+  else
+    redirect '/login'
+  end
+end
+
+get '/logout' do
+  session.clear
+  @title = 'logout'
+  erb :logout
+end
+
+get '/login' do
+  @title = 'login'
+  erb :login
+end
+
+post '/login' do
+  code = 0
+  data = nil
+  message = ""
+
+  userName = params[:user_name]
+  userPass = params[:user_pass]
+
+  if (userName.empty?)
+    code = -21
+    message = message + "UserNameが空欄です。"
+  end
+  if (userPass.empty?)
+    code = -22
+    message = message + "Passwordが空欄です。"
+  end
+
+  if code == 0
+    user = User.find_by(user_name: userName)
+    if user == nil
+      code = -41
+      message = "ユーザーが存在しないか、パスワードが間違っています。"
+    else
+      if user.algo == "1"
+        trialHashed = Digest::MD5.hexdigest(user.salt + userPass)
+        if trialHashed == user.hashed
+          code = 1
+          message = "ログイン成功"
+        else
+          code = -41
+          message = "ユーザーが存在しないか、パスワードが間違っています。"
+        end
+      else
+        code = -42
+        message = "Not Found hash algorithm."
+      end
+    end
+  end
+
+  if code == 1
+    session[:login_flag] = true
+    session[:user_name] = userName
+  else
+    session[:login_flag] = false
+  end
+
+  data = {
+    code: code,
+    data: data,
+    message: message
+  }
+  data.to_json
+end
+
+get '/new_user' do
+  @tilte = 'new_user'
+  erb :new_user
+end
+
+post '/new_user' do
+  code = 0
+  data = nil
+  message = ""
+
+  userName = params[:user_name]
+  userPass = params[:user_pass]
+  userRePass = params[:user_repass]
+
+  arrayUserName = User.pluck(:user_name)
+
+  if (userName.empty?)
+    code = -21
+    message = message + "UserNameが空欄です。"
+  end
+  if (userPass.empty?)
+    code = -22
+    message = message + "Passwordが空欄です。"
+  end
+  if (userRePass.empty?)
+    code = -23
+    message = message + "Password再入力が空欄です。"
+  end
+
+  if code == 0
+    if (userName.match(/[^a-zA-Z1-9_]/) == nil)
+      if (userPass.match(/[^a-zA-Z1-9_]/) == nil)
+        if (userPass == userRePass)
+          if (arrayUserName.include?(userName))
+            code = -31
+            message ="すでに使用されているUserNameです。"
+          else
+            newUser = User.new
+            newUser.id = ((User.last.nil? ? 0 : User.last.id) + 1)
+            newUser.user_name = userName
+
+            random = Random.new
+            salt = Digest::MD5.hexdigest(random.bytes(20))
+            hashed = Digest::MD5.hexdigest(salt + userPass)
+            algo = 1
+
+            newUser.salt = salt
+            newUser.hashed = hashed
+            newUser.algo = algo
+
+            nowDateTime = DateTime.now
+            strDT = nowDateTime.strftime("%Y-%m-%d %H:%M:%S")
+
+            newUser.created_at = strDT
+            newUser.updated_at = strDT
+
+            newUser.save
+            code = 1
+            message = "新規User作成 成功。ログインタブよりログインしてください。"
+          end
+        else
+          code = -11
+          message = "パスワード再入力が一致しません"
+        end
+      else
+        code = -12
+        message = "password : 半角英数字と_（アンダーバー）のみ使用可能"
+      end
+    else
+      code = -13
+      message = "username : 半角英数字と_（アンダーバー）のみ使用可能"
+    end
+  end
+
+  data = {
+    code: code,
+    data: data,
+    message: message
+  }
+  data.to_json
+end
---
+++ public/js/js.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+  var $alertSignUp = $('.alert-sign-up');
+  var $alertLogin = $('.alert-login');
+
+  function postUser(data) {
+    return $.ajax({
+        type: 'POST',
+        url:  '/new_user',
+        data : data,
+        dataType: "json"
+    })
+  }
+
+  function postLogin(data) {
+    return $.ajax({
+        type: 'POST',
+        url:  '/login',
+        data : data,
+        dataType: "json"
+    })
+  }
+
+  $('.btn-sign-up').on('click', function (){
+    var serialized = $('.sign-up-form').serialize();
+
+    postUser(serialized).done(function(result) {
+        console.log("done");
+        console.log(result);
+        if (result.code < 0) {
+            $alertSignUp.removeClass('alert-success');
+            $alertSignUp.addClass('alert-warning');
+            // $alertSignUp.find('.alert-message').empty();
+            $alertSignUp.find('.alert-message').html(result.message);
+            $alertSignUp.show();
+        } else {
+            $alertSignUp.removeClass('alert-warning');
+            $alertSignUp.addClass('alert-success');
+            // $alertSignUp.find('.alert-message').empty();
+            $alertSignUp.find('.alert-message').html(result.message);
+            $alertSignUp.show();
+        }
+    })
+    .fail(function() {
+        console.log( "error" );
+    })
+    .always(function() {
+        console.log( "always" );
+    });
+  });
+
+  $('.btn-login').on('click', function (){
+    var serialized = $('.login-form').serialize();
+
+    postLogin(serialized).done(function(result) {
+        console.log("done");
+        console.log(result);
+        if (result.code < 0) { //ログイン失敗
+            $alertLogin.removeClass('alert-success');
+            $alertLogin.addClass('alert-warning');
+            // $alertLogin.find('.alert-message').empty();
+            $alertLogin.find('.alert-message').html(result.message);
+            $alertLogin.show();
+        } else { // ログイン成功
+            $alertLogin.removeClass('alert-warning');
+            $alertLogin.addClass('alert-success');
+            // $alertLogin.find('.alert-message').empty();
+            $alertLogin.find('.alert-message').html(result.message);
+            $alertLogin.show();
+            window.location.href = "/menu";
+        }
+    })
+    .fail(function() {
+        console.log( "error" );
+    })
+    .always(function() {
+        console.log( "always" );
+    });
+  });
+
+  $('.alert-close').on('click', function () {
+    console.log('here');
+    var $this = $(this);
+    $this.parents('.alert').hide();
+  })
+});
